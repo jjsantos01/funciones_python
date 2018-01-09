e2d00b00cfa46d55eb98d30177a9e9ed04708ac3
Juan Santos Ochoa <jjsantosochoa@outlook.com>
2018-01-09 11:24:45 -0600

    agrega findall

--- reand.py
+++ reand.py
@@ -1,4 +1,4 @@
-def reand(reg):
+def search_and(reg):
   import re
   '''Esta función genera el operador & en una expresión regular.
   El input de la función son las palabras o re que quieren ser encontradas en conjunto, separadas por el símbolo &.
@@ -8,3 +8,15 @@ def reand(reg):
     inte = '|'.join(['.*'+y for y in x.split('|')])
     out += '(?={})'.format(inte)
   return out
+def findall_and(pat,text):
+    '''Esta función genera el operador & en una expresión regular para find all y regresa todos los matches.
+    El input de la función son las palabras o re que quieren ser encontradas en conjunto, separadas por el símbolo &.
+    También pueden estar separadas por |'''
+    out = []
+    for x in pat.replace(' ','').split('&'):
+        if re.findall(x, text):
+            out += re.findall(x, text)
+        else:
+            out = []
+            break
+    return out
